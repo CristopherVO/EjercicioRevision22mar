835e48aada6d61410d2a52326cdf9bb2ed29bf39
crist <cristopherozzy17@gmail.com>
2024-03-22 19:40:22 -0600

    update: se sube ejercicio revision

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,31 @@
+
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+// le faltaban los # 
+const $n = document.querySelector('#name');
 const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $l = document.querySelector('#location');
 
-function displayUser(username) {
+// agregue el async que faltaba para el await 
+async function  displayUser (username) {
+  // agregar el try y el catch
+  try{
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
   $n.textContent = '${data.name}';
   $b.textContent = '${data.blog}';
   $l.textContent = '${data.location}';
+  }catch (err){
+    handleError(err);
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  // le faltaba el punto y coma, agregar el $
+  //$n.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
